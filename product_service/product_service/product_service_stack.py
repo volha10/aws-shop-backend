e2506e39b4483fc1b6bdf13a5c8b2184e4a8d54c
Volha Dziatko <volha10.dziatko@gmail.com>
2025-02-26 14:34:00 +0100

    feat: add endpoint to get products list

--- product_service/product_service/product_service_stack.py
+++ product_service/product_service/product_service_stack.py
@@ -1,19 +1,47 @@
 from aws_cdk import (
-    # Duration,
+    CfnOutput,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigatewayv2 as api_gateway,
+    aws_apigatewayv2_integrations as integrations,
+    aws_lambda as lambda_,
 )
 from constructs import Construct
+
 
 class ProductServiceStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        get_products_list_function = lambda_.Function(
+            self,
+            "GetProductsListLambda",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            handler="get_products_list.lambda_handler",
+            code=lambda_.Code.from_asset("lambda"),
+            timeout=Duration.seconds(10),
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ProductServiceQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define API Gateway
+
+        api = api_gateway.HttpApi(
+            self,
+            "ProductServiceApi",
+            api_name="ProductServiceAPI",
+            description="HTTP API for Product Service",
+        )
+
+        api.add_routes(
+            path="/products",
+            methods=[api_gateway.HttpMethod.GET],
+            integration=integrations.HttpLambdaIntegration(
+                id="get_products_list",
+                handler=get_products_list_function
+            ),
+        ),
+
+        CfnOutput(self, "HttpApiUrl", value=api.url or "Unknown")
+
+
+
