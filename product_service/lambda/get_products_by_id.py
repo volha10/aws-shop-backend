aba03db46a49184c6e2016421f2c50ee4f3dad99
Volha Dziatko <volha10.dziatko@gmail.com>
2025-02-26 16:56:06 +0100

    feat: add endpoint to get product by id

--- product_service/lambda/get_products_by_id.py
+++ product_service/lambda/get_products_by_id.py
@@ -10,15 +10,13 @@ def lambda_handler(event, context):
     logger.info(f"CloudWatch logs group: {context.log_group_name}")
     logger.info(f"event={event}")
 
-    # product_id = event["pathParameters"]
-
     path_params = event.get("pathParameters")
     product_id = path_params.get("productId")
 
     print(f"product_id={product_id}")
 
     if not product_id:
-        raise Exception(f"Product id is not provided.")
+        raise Exception(f"productId is not provided.")
 
     product = next((p for p in PRODUCTS if p["id"] == product_id), None)
 
@@ -27,8 +25,6 @@ def lambda_handler(event, context):
             "statusCode": 200,
             "body": json.dumps(product)
         }
-
-    print(f"product={product}")
 
     return {
         "statusCode": 404,
