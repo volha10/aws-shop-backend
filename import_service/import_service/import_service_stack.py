2eccc2480cf225b897d7844580c426551dfbfb05
Volha Dziatko <volha10.dziatko@gmail.com>
2025-03-08 20:10:57 +0100

    feat: initialize import service

---
+++ import_service/import_service/import_service_stack.py
@@ -0,0 +1,39 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_apigatewayv2 as api_gateway,
+    aws_apigatewayv2_integrations as integrations,
+    aws_lambda as lambda_,
+)
+from constructs import Construct
+
+S3_BUCKET_NAME = "uploaded"
+
+
+class ImportServiceStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+
+        import_function = lambda_.Function(
+            self, "ImportProductsLambda",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            handler="import_products.lambda_handler",
+            code=lambda_.Code.from_asset("lambda_func"),
+            environment={
+                "S3_BUCKET_NAME": S3_BUCKET_NAME,
+            }
+        )
+
+        api = apigateway.HttpApi(
+            self, "ImportServiceAPI",
+        )
+
+        api.add_routes(
+            path="/import",
+            methods=[apigateway.HttpMethod.GET],
+            integration=integrations.HttpLambdaIntegration(
+                "ImportLambdaIntegration", import_function
+            )
+        )
